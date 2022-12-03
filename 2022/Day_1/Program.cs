2f8de56ea6a8d8b39f1b948f37ee7b02461d5877
Diyorbek25 <diyorbek2506@gmail.com>
2022-12-03 23:19:42 +0500

    Add Day 1 folder

---
+++ 2022/Day_1/Program.cs
@@ -0,0 +1,24 @@
+﻿
+// int[] input = File.ReadAllLines("Input.txt").Select(t => Convert.ToInt32(t)).ToArray();
+string[] input = File.ReadAllLines(@"C:\Users\hp\Desktop\AdventofCode.com\2022\Day_1\input.txt");
+
+int result1 = TotalCalories(input);
+int result2 = TotalCalories(input);
+
+Console.WriteLine($"Result 1: {result1}");
+Console.WriteLine($"Result 2: {result2}");
+
+
+
+
+
+
+
+
+// Part One
+static int TotalCalories(string[] input)
+{
+
+    return 1;
+}
+
