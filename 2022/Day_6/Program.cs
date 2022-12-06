33e103caeae326d905c51772ee8b00c86b1828be
Diyorbek25 <diyorbek2506@gmail.com>
2022-12-06 11:45:18 +0500

    Solution: Day 6

---
+++ 2022/Day_6/Program.cs
@@ -0,0 +1,32 @@
+﻿
+string input = File.ReadAllText(@"Append path...");
+
+ /* --- Part One --- **/
+Console.WriteLine(Part(input, 4));
+
+/* --- Part Two --- **/
+Console.WriteLine(Part(input, 14));
+
+
+
+
+static int Part(string input, int markerLen)
+{
+	for (int i = 0; i <= input.Length - markerLen; i++)
+	{
+		string str = input.Substring(i, markerLen);
+		if (CheckMarker(str))
+			return i + markerLen;
+	}
+	return -1;
+}
+
+static bool CheckMarker(string str)
+{
+	HashSet<char> set = new HashSet<char>();
+	for (int i = 0; i < str.Length; i++)
+	{
+		set.Add(str[i]);
+	}
+	return set.Count == str.Length;
+}
